6708059d718616ba5fda99997fcf403ad358cef1
Kyle Wilcox <kyle@axiomdatascience.com>
2016-09-06 12:13:00 -0400

    setup.py cleanup

--- setup.py
+++ setup.py
@@ -1,20 +1,42 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+def version():
+    with open('VERSION') as f:
+        return f.read().strip()
+
+
+reqs = [line.strip() for line in open('requirements.txt') if not line.startswith('#')]
+
 
 setup(
     name='gdam',
-    version='1.0',
+    version=version(),
+    description='A MongoDB store for Teledyne Webb Gliders.',
+    long_description=readme(),
+    license='MIT',
     author='Michael Lindemuth',
     author_email='mlindemu@usf.edu',
+    install_requires=reqs,
+    url='https://github.com/axiom-data-science/GDAM',
     packages=['gdam'],
-    install_requires=[
-        'gbdr',
-        'pyinotify',
-        'pymongo',
-        'pyzmq',
+    entry_points = {
+        'console_scripts': ['gdam-cli=gdam.cli:main'],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering'
     ],
-    scripts=[
-        'gdam/cli.py',
-    ]
 )
